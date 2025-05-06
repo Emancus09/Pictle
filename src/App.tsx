984ac10dbc9a27cc733437c92a4b9550b6bf17b8
Emanuel Sharma <send.emanuel@hotmail.com>
2025-05-05 23:49:48 -0400

    Basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import MovieCard from './movie-card';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchText, setSearchText] = useState('');
+
+  const [movieIds, setMovieIds] = useState<string[]>([]);
+
+  const onEnterKeyDown = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDI2ZWE3MGMwNDQ0NjUwOWFhMjExODE3ZjA1MjdkNSIsIm5iZiI6MS43NDY0OTg2NjkxODU5OTk5ZSs5LCJzdWIiOiI2ODE5NzQ2ZDU1MGE5YjViYTNjMzc4OGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UAyM5ZWqpNdQomhty-4HlFn9MxBnN4MOzS_plJ6opOs'
+      }
+    };
+        
+    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText}&include_adult=false&language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setMovieIds([res.results[0].id, ...movieIds]))
+      .catch(err => console.error(err));
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col items-center gap-4 p-8 w-fit m-auto'>
+      <div className='bg-gray-100 rounded flex flex-row p-4 gap-4 w-full'>
+        <label htmlFor="movie-search">Name a movie:</label>
+        <input type="search" id="movie-search" name="q" className='flex-grow' onChange={(e) => setSearchText(e.target.value)} value={searchText} onKeyDown={(e) => {if (e.key === 'Enter') onEnterKeyDown()}}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {movieIds.map(id => <MovieCard movieId={id} key={id}/>)}
+    </div>
   )
 }
 
---
+++ src/movie-card.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+function MovieCard(props: {movieId: string}) {
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieDate, setMovieDate] = useState('');
+  const [movieGenre, setMovieGenre] = useState('');
+  const [movieCountryOfOrigin, setMovieCountryOfOrigin] = useState('');
+  const [movieThumbnail, setMovieThumbnail] = useState('https://placehold.co/600x400');
+  const [movieDirector, setMovieDirector] = useState('');
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDI2ZWE3MGMwNDQ0NjUwOWFhMjExODE3ZjA1MjdkNSIsIm5iZiI6MS43NDY0OTg2NjkxODU5OTk5ZSs5LCJzdWIiOiI2ODE5NzQ2ZDU1MGE5YjViYTNjMzc4OGEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UAyM5ZWqpNdQomhty-4HlFn9MxBnN4MOzS_plJ6opOs'
+    }
+  };
+
+  fetch(`https://api.themoviedb.org/3/movie/${props.movieId}?language=en-US`, options)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      setMovieThumbnail(`https://image.tmdb.org/t/p/w500${res.poster_path}`);
+      setMovieTitle(res.title);
+      setMovieDate(res.release_date);
+      setMovieGenre(res.genres[0].name);
+      setMovieCountryOfOrigin(res.origin_country[0]);
+    })
+    .catch(err => console.error(err));
+    
+    fetch(`https://api.themoviedb.org/3/movie/${props.movieId}/credits?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        setMovieDirector(res.crew.find(e => e.job==='Director').name);
+      })
+      .catch(err => console.error(err));
+
+  return (
+    <div className='rounded bg-gray-100 grid grid-cols-2 grid-rows-4 gap-4 p-4'>
+     <div className='bg-gray-300 rounded text-center content-center text-4xl truncate w-64'>{movieTitle}</div>
+     <div className='bg-gray-300 rounded text-center content-center'>{movieDirector}</div>
+     <img className="w-64 row-span-4 bg-gray-300 rounded" src={movieThumbnail}/>
+     <div className='bg-gray-300 rounded text-center content-center'>{movieDate}</div>
+     <div className='bg-gray-300 rounded text-center content-center'>{movieGenre}</div>
+     <div className='bg-gray-300 rounded text-center content-center'>{movieCountryOfOrigin}</div>
+    </div>
+  )
+}
+
+export default MovieCard
